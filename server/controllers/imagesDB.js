9b5a7248dd77408f89594c0c4b5521457dca04b9
StephenLespron <stephen.lespron@gmail.com>
2020-05-22 13:05:54 -0700

    backend complete

---
+++ server/controllers/imagesDB.js
@@ -0,0 +1,148 @@
+module.exports = [
+  {
+    parkName: "Camden Yards",
+    imgURL:
+      "https://i.guim.co.uk/img/media/9008715027da3a0c453d9f1ec30993936ff4cd2d/0_178_3000_1800/master/3000.jpg?width=300&quality=85&auto=format&fit=max&s=990da22c94dbf05ccb1c91eaa3119619",
+  },
+  {
+    parkName: "Fenway Park",
+    imgURL:
+      "https://media-cdn.tripadvisor.com/media/photo-s/04/06/f7/4f/view-from-the-club.jpg",
+  },
+  {
+    parkName: "Yankee Stadium",
+    imgURL: "https://www.myrtlebeachcoastalproperties.com/img/yankeemain.0.jpg",
+  },
+  {
+    parkName: "Tropicana Field",
+    imgURL:
+      "https://s3.amazonaws.com/arc-wordpress-client-uploads/tbt/wp-content/uploads/2018/03/27181024/TP_402547_ELLI_32_sp_Openin.jpg",
+  },
+  {
+    parkName: "Rogers Centre",
+    imgURL:
+      "https://bluejaysnation.com/wp-content/uploads/sites/8/2017/10/rcgrass.jpg",
+  },
+  {
+    parkName: "Marlins Park",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Marlins_First_Pitch_at_Marlins_Park%2C_April_4%2C_2012_%28cropped%29.jpg/300px-Marlins_First_Pitch_at_Marlins_Park%2C_April_4%2C_2012_%28cropped%29.jpg",
+  },
+  {
+    parkName: "SunTrust Park",
+    imgURL:
+      "https://exploregwinnettorg.s3.amazonaws.com/_770x386_crop_center-center/suntrustpark.jpg",
+  },
+  {
+    parkName: "Citi Field",
+    imgURL:
+      "https://elitesportsny.com/wp-content/uploads/2018/03/cincinnati-reds-v-new-york-mets-1-e1522096086300.jpg",
+  },
+  {
+    parkName: "Citizens Bank Park",
+    imgURL:
+      "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/citbank_topv2.jpg",
+  },
+  {
+    parkName: "Nationals Park",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Opening_of_Nationals_Park_-_039_%282377924697%29.jpg/300px-Opening_of_Nationals_Park_-_039_%282377924697%29.jpg",
+  },
+  {
+    parkName: "Wrigley Field",
+    imgURL:
+      "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fbarrymbloom%2Ffiles%2F2019%2F05%2FtexR6YFeQWa7T22Kvbwig-e1558668024645-1200x900.jpg",
+  },
+  {
+    parkName: "Great American Ballpark",
+    imgURL:
+      "https://images-na.ssl-images-amazon.com/images/I/61UpepHJ80L._AC_.jpg",
+  },
+  {
+    parkName: "Miller Park",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/MillerParkJuly2018.jpg/1200px-MillerParkJuly2018.jpg",
+  },
+  {
+    parkName: "PNC Park",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Pittsburgh_Pirates_park_%28Unsplash%29.jpg/300px-Pittsburgh_Pirates_park_%28Unsplash%29.jpg",
+  },
+  {
+    parkName: "Busch Stadium",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/BuschStadium_2006-05-30.jpg/300px-BuschStadium_2006-05-30.jpg",
+  },
+  {
+    parkName: "Guaranteed Rate Field",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e2/U.S._Cellular_Field_%2830972191694%29.jpg",
+  },
+  {
+    parkName: "Progressive Field",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Progressive_Field%2C_June_2019_%285%29.jpg/300px-Progressive_Field%2C_June_2019_%285%29.jpg",
+  },
+  {
+    parkName: "Comerica Park",
+    imgURL:
+      "https://i.guim.co.uk/img/media/9008715027da3a0c453d9f1ec30993936ff4cd2d/0_178_3000_1800/master/3000.jpg?width=300&quality=85&auto=format&fit=max&s=990da22c94dbf05ccb1c91eaa3119619",
+  },
+  {
+    parkName: "Kauffman Stadium",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/4/47/NewKauffman.jpg",
+  },
+  {
+    parkName: "Target Field",
+    imgURL:
+      "https://i.guim.co.uk/img/media/9008715027da3a0c453d9f1ec30993936ff4cd2d/0_178_3000_1800/master/3000.jpg?width=300&quality=85&auto=format&fit=max&s=990da22c94dbf05ccb1c91eaa3119619",
+  },
+  {
+    parkName: "Minute Maid Park",
+    imgURL:
+      "https://www.ballparksofbaseball.com/wp-content/uploads/2016/03/minute_topv3.jpg",
+  },
+  {
+    parkName: "Angels Stadium",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/4/4a/Angelstadiummarch2019.jpg",
+  },
+  {
+    parkName: "Oakland Coliseum",
+    imgURL: "https://s.hdnux.com/photos/72/25/75/15294301/31/rawImage.jpg",
+  },
+  {
+    parkName: "Safeco Field",
+    imgURL:
+      "https://senecagroup.com/wp-content/uploads/2017/04/slide-master_0011s_0000_1999_safeco_007.jpg",
+  },
+  {
+    parkName: "Globe Life Park",
+    imgURL:
+      "https://assets.dmagstatic.com/wp-content/uploads/2019/03/IMG_7950.jpg",
+  },
+  {
+    parkName: "Chase Field",
+    imgURL: "https://phoenix.org/wp-content/uploads/2017/02/6885553_m.jpg",
+  },
+  {
+    parkName: "Coors Field",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Coors_Field_July_2015.jpg/275px-Coors_Field_July_2015.jpg",
+  },
+  {
+    parkName: "Dodger Stadium",
+    imgURL:
+      "https://i.guim.co.uk/img/media/9008715027da3a0c453d9f1ec30993936ff4cd2d/0_178_3000_1800/master/3000.jpg?width=300&quality=85&auto=format&fit=max&s=990da22c94dbf05ccb1c91eaa3119619",
+  },
+  {
+    parkName: "Petco Park",
+    imgURL:
+      "https://stadiumjourney.com/wp-content/uploads/2016/08/Sunday-Baseball-900x675.jpg",
+  },
+  {
+    parkName: "At&T Park",
+    imgURL: "https://uproxx.com/wp-content/uploads/2019/02/attpark.jpg?w=650",
+  },
+];
---
+++ server/controllers/park-visits.js
@@ -0,0 +1,130 @@
+const parkImgUrl = require("./imagesDB");
+
+const initialVisits = [
+  {
+    id: 1,
+    parkName: `Chase Field`,
+    parkImg: `img.url`,
+    date: { year: 2017, month: 10, day: 4 },
+    teams: { home: `Diamondbacks`, away: `Rockies` },
+    notes: `Game was amazing! Maybe had too much to drink. Energy was high as expected for a one and done playoff game. Archie's triple really set the tone.`,
+    rating: 10,
+  },
+  {
+    id: 2,
+    parkName: `Petco Park`,
+    parkImg: `img.url`,
+    date: { year: 2014, month: 6, day: 27 },
+    teams: { home: `Padres`, away: `Diamondbacks` },
+    notes: `It has been a rough season for the Dbacks, but they managed to pull this one through. Embarrassingly, there were some Dbacks fans behind us that also made the trip. They felt the need to heckle the home team. Please don't do this. We sat next to some awesome locals that pointed us to some great craft beer and a BBQ stand behind center field.`,
+    rating: 6.5,
+  },
+  {
+    id: 3,
+    parkName: `Minute Maid Park`,
+    parkImg: `img.url`,
+    date: { year: 2017, month: 10, day: 4 },
+    teams: { home: `Astros`, away: `Mariners` },
+    notes: `This is the only game I have attended the DBacks didn't play in. We were in Texas for a baseball tournament and have been rained out. We took a day to visit the local team. MM Park is bland. The train feature is ok. Just feels like playing ball in a gym.`,
+    rating: 4,
+  },
+  {
+    id: 4,
+    parkName: `Camden Yards`,
+    parkImg: `img.url`,
+    date: { year: 2016, month: 9, day: 23 },
+    teams: { home: `Orioles`, away: `Diamonbacks` },
+    notes: `We did not win, but Baltimore is a beautiful city. This trip has sentimental feeling attached as my dad grew up an Orioles fan and then transitioned to the Dbacks when they were established. He got to meet his idol, Boog Powell. Food was amazing. The fans were great. Can't wait to come back.  `,
+    rating: 9.5,
+  },
+  {
+    id: 5,
+    parkName: `Comerica Park`,
+    parkImg: `img.url`,
+    date: { year: 2017, month: 6, day: 13 },
+    teams: { home: `Tigers`, away: `Diamondbacks` },
+    notes: `I am in Detroit for "work". My buddy has a crush on this girl we work with, except she is in the Detroit office. So we forced a business trip for us to work the week here. I made sure the trip was this week so I could go to the game. The park is pretty. Food is baseball food. Beer is above average. Coney Island is amazing.`,
+    rating: 6,
+  },
+];
+
+let parkVisits = initialVisits.map((parks) => {
+  for (let i = 0; i < parkImgUrl.length; i++) {
+    if (parks.parkName === parkImgUrl[i].parkName) {
+      parks.parkImg = parkImgUrl[i].imgURL;
+    }
+  }
+  return parks;
+});
+
+let id = 6;
+
+function updateImgs(parkId) {
+  const index = parkVisits.findIndex((elem) => elem.id === parkId);
+  for (let i = 0; i < parkImgUrl.length; i++) {
+    if (parkVisits[index].parkName === parkImgUrl[i].parkName) {
+      return (parkVisits[index].parkImg = parkImgUrl[i].imgURL);
+    }
+  }
+  return parkVisits;
+}
+
+module.exports = {
+  getPV: (req, res) => {
+    res.status(200).send(parkVisits);
+  },
+
+  newPV: (req, res) => {
+    const { parkName, date, teams, notes, rating } = req.body;
+
+    const newVisit = { id, parkName, parkImg: ``, date, teams, notes, rating };
+
+    parkVisits.push(newVisit);
+
+    console.log(parkVisits);
+
+    updateImgs(newVisit.id);
+
+    id++;
+
+    res.status(200).send(parkVisits);
+  },
+  updatePV: (req, res) => {
+    const { pv_id } = req.params;
+    const { parkName, date, teams, notes, rating } = req.body;
+
+    const index = parkVisits.findIndex((elem) => elem.id === +pv_id);
+
+    console.log(index);
+
+    if (index === -1) {
+      return res.status(404).send(`No visit listed`);
+    }
+
+    const updatedVisit = {
+      id: pv_id,
+      parkName,
+      parkImg: ``,
+      date,
+      teams,
+      notes,
+      rating,
+    };
+
+    parkVisits.splice(index, 1, updatedVisit);
+
+    updateImgs(updatedVisit.id);
+
+    res.status(200).send(parkVisits);
+  },
+
+  deletePV: (req, res) => {
+    const { pv_id } = req.params;
+
+    const index = parkVisits.findIndex((elem) => elem.id === +pv_id);
+
+    parkVisits.splice(index, 1);
+
+    res.status(200).send(parkVisits);
+  },
+};
