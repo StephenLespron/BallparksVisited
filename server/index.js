eb7bced5a94145b21cb71f3135fff0b83f6b2cbc
StephenLespron <stephen.lespron@gmail.com>
2020-08-03 18:39:49 -0700

    added static for build

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const ctrlPV = require("./controllers/park-visits.js");
+const ctrlPV = require('./controllers/park-visits.js');
 const SERVER_PORT = 4256;
+
+app.use(express.static(`${__dirname}/../build`));
 
 app.use(express.json());
 
 //Endpoints for list
 
-app.get("/api/parkVisits", ctrlPV.getPV);
-app.post("/api/parkVisits", ctrlPV.newPV);
-app.put("/api/parkVisits/:pv_id", ctrlPV.updatePV);
-app.delete("/api/parkVisits/:pv_id", ctrlPV.deletePV);
+app.get('/api/parkVisits', ctrlPV.getPV);
+app.post('/api/parkVisits', ctrlPV.newPV);
+app.put('/api/parkVisits/:pv_id', ctrlPV.updatePV);
+app.delete('/api/parkVisits/:pv_id', ctrlPV.deletePV);
 
 app.listen(SERVER_PORT, () =>
-  console.log(`Root, root, root for the home team on port ${SERVER_PORT}`)
+	console.log(`Root, root, root for the home team on port ${SERVER_PORT}`)
 );
